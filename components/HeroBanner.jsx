68a16edb8a47cd356548e3558f2f125e7560b0c6
Aravind Kumar <52557958+ARAVIND7778428@users.noreply.github.com>
2022-10-19 18:11:16 +0530

    Update HeroBanner.jsx

--- components/HeroBanner.jsx
+++ components/HeroBanner.jsx
@@ -20,6 +20,7 @@ const HeroBanner = ({ heroBanner }) => {
                 </Link>
                 <div className='desc'>
                     <h5>Description</h5>
+                    <h5>Description</h5>
                     <p>{ heroBanner.desc }</p>
                 </div>
             </div>
