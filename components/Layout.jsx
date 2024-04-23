906b12b0a2fad3bb397565fa4a230c6054ed80f8
Aravind Kumar <52557958+ARAVIND7778428@users.noreply.github.com>
2024-04-23 11:24:02 +0530

    Update Layout.jsx

--- components/Layout.jsx
+++ components/Layout.jsx
@@ -11,7 +11,7 @@ const Layout = ({ children }) => {
   return (
     <div className='layout'>
       <Head>
-        <meta name='impact-site-verification' value='a7c984a3-b2c8-499f-bf0d-9994aa11dfda'>
+        <meta name='impact-site-verification' value='a7c984a3-b2c8-499f-bf0d-9994aa11dfda'></meta>
         <title>E-commmerce Store</title>
       </Head>
       <header>
